f59419ed94be1e4adabf98d8d14a82154be3d080
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-28 12:55:21 +0300

    list get example

--- src/main/java/com/colak/springtutorial/controller/get/hello/HelloController.java
+++ src/main/java/com/colak/springtutorial/controller/get/hello/HelloController.java
@@ -2,6 +2,7 @@ package com.colak.springtutorial.controller.get.hello;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,6 +11,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 // Controller that returns custom type
 @RestController
@@ -29,5 +32,24 @@ public class HelloController {
     @GetMapping("/sayhello")
     public HelloResponse sayHello() {
         return new HelloResponse("welcome to hello world");
+    }
+
+    @Operation(
+            summary = "Retrieve all accounts",
+            description = "Fetches a list of all HelloResponses",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HelloResponses returned",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = HelloResponse.class))
+                            )
+                    )
+            }
+    )
+    @GetMapping("/sayHelloList")
+    public List<HelloResponse> sayHelloList() {
+        return List.of(new HelloResponse("welcome to hello world"));
     }
 }
